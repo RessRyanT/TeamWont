082cef8d9926d813257335a9e77a5600169a5724
wtw1820 <wtw1820@MSS2175-24>
2019-10-16 10:53:34 -0400

    made a scene to test stuff in

--- Ethtallion’s Journey/Assets/SpellManagerScript.cs
+++ Ethtallion’s Journey/Assets/SpellManagerScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpellManagerScript : MonoBehaviour
 {
+
+    int effectRadius;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,7 +36,7 @@ public class SpellManagerScript : MonoBehaviour
             case 226: // Fireball
               
                 Debug.Log("Fireball Cast");
-             
+                
                 break;
             case 232: // Gust
                 
