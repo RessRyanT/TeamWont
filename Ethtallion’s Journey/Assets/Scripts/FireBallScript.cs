79744a12b065033b4cf50996db12392e3dfdbe2d
Pancakes42 <36553713+Pancakes42@users.noreply.github.com>
2019-10-30 10:42:29 -0400

    saaaaaaaaaah

--- Ethtallion’s Journey/Assets/Scripts/FireBallScript.cs
+++ Ethtallion’s Journey/Assets/Scripts/FireBallScript.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class FireBallScript : MonoBehaviour
 {
     public float fireballSpeed;
+
+    private Rigidbody2D myRigidBody;
+
+    public Vector3 myVelocity;
+    private void Start()
+    {
+        myRigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+        myVelocity = new Vector3(fireballSpeed, 0, 0);
+    }
+
+    private void Update()
+    {
+        myRigidBody.velocity = myVelocity;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         collision.collider.SendMessage("FireballHit", SendMessageOptions.DontRequireReceiver);
         Destroy(gameObject);
     }
+
+    public void SetLeft()
+    {
+        myVelocity = new Vector3(-fireballSpeed, 0, 0);
+    }
+
 }
