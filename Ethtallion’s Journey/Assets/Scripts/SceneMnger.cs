f2e7bfb5c2379a0b44585086501b358c5d8fbd13
Will Walrond (RIT Student) <wtw1820@ad.rit.edu>
2019-10-31 17:36:19 -0400

    touched up stuff for finished build

--- Ethtallion’s Journey/Assets/Scripts/SceneMnger.cs
+++ Ethtallion’s Journey/Assets/Scripts/SceneMnger.cs
@@ -9,10 +9,17 @@ public class SceneMnger : MonoBehaviour
     public string[] sceneList = { "MainMenu", "IntroLevel" };
     public int currentSceneIndex;
 
+    GameObject kirby;
+    Camera mainCamera;
+    Vector3 kirbposition;
 
+    float vertExtent;
+    float horzExtent;
     // Start is called before the first frame update
     void Start()
     {
+        kirby = GameObject.FindObjectOfType<Wizard>().gameObject;
+        mainCamera = GameObject.FindObjectOfType<Camera>();
         currentScene = SceneManager.GetActiveScene().name;
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
@@ -20,7 +27,12 @@ public class SceneMnger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         if(kirby.transform.position.y > 20 || kirby.transform.position.y < -20 || Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(currentSceneIndex);
+            
+        }
+
     }
 
     public void NextLevel()
@@ -31,5 +43,10 @@ public class SceneMnger : MonoBehaviour
     public void SelectLevel()
     {
 
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
---
+++ Ethtallion’s Journey/Assets/Scripts/creditscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditscript : MonoBehaviour
+{
+
+    public int speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y >= 26)
+        {
+            Application.Quit();
+        }
+
+        Vector3 newPosition = gameObject.transform.position;
+        newPosition.y += speed * Time.deltaTime;
+        gameObject.transform.position = newPosition;
+    }
+}
