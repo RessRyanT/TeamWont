1653caf18684bf68b11ae66e691f9eeb417efe4b
Will Walrond (RIT Student) <wtw1820@ad.rit.edu>
2019-10-31 13:11:21 -0400

    did work on new level, made an electrical switch

---
+++ Ethtallion’s Journey/Assets/Scripts/EnergySwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergySwitch : MonoBehaviour
+{
+
+    public GameObject target;
+    int timezap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timezap = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnZap(int s_timezap)
+    {
+        //checks if this object has been zapped by this specific spell before
+        if(s_timezap != timezap)
+        {
+            //if not, sets id of timezap
+            timezap = s_timezap;
+
+            //toggles between blue and white, corresponding to on/off
+            if (target.activeSelf)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+
+
+            //then either removes or reactivates the target object
+            target.SetActive(!target.activeSelf);
+        }
+        
+    }
+}
